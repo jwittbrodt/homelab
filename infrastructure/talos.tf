6300d604487491d2bafdaf8126a58db3b0529147
Jonas Wittbrodt <36197762+jwittbrodt@users.noreply.github.com>
2025-10-24 07:49:09 +0200

    HA controlplane endpoint

--- infrastructure/talos.tf
+++ infrastructure/talos.tf
@@ -15,8 +15,10 @@ locals {
     }
   }
 
-  nodes            = { for hostnum, node in local.node_config : hostnum => merge(node, { "ip" = cidrhost(local.local_subnet, hostnum) }) }
-  cluster_endpoint = "https://${local.nodes[201].ip}:6443" # TODO: do actual HA
+  nodes = { for hostnum, node in local.node_config : hostnum => merge(node, { "ip" = cidrhost(local.local_subnet, hostnum) }) }
+
+  controlplane_vip = cidrhost(local.local_subnet, 200)
+  cluster_endpoint = "https://${local.controlplane_vip}:6443"
 }
 
 resource "talos_machine_secrets" "this" {}
@@ -43,11 +45,26 @@ data "talos_machine_configuration" "worker" {
 
 locals {
   controlplane_config_patches = [
-    {
+    yamlencode({
       cluster = {
         allowSchedulingOnControlPlanes = true
       }
-    }
+    }),
+    yamlencode({
+      machine = {
+        network = {
+          interfaces = [
+            {
+              interface = "enp1s0"
+              dhcp      = true
+              vip = {
+                ip = local.controlplane_vip
+              }
+            }
+          ]
+        }
+      }
+    })
   ]
   worker_config_patches = []
 }
@@ -58,7 +75,7 @@ resource "talos_machine_configuration_apply" "nodes" {
   client_configuration        = talos_machine_secrets.this.client_configuration
   machine_configuration_input = each.value.is_controlplane ? data.talos_machine_configuration.controlplane.machine_configuration : data.talos_machine_configuration.worker.machine_configuration
   node                        = each.value.ip
-  config_patches = [for patch in concat([
+  config_patches = concat([yamlencode(
     {
       machine = {
         install = {
@@ -68,9 +85,8 @@ resource "talos_machine_configuration_apply" "nodes" {
           hostname = join("-", compact([data.talos_client_configuration.this.cluster_name, each.value.is_controlplane ? "cp" : "", each.key]))
         }
       }
-    }],
-    each.value.is_controlplane ? local.controlplane_config_patches : local.worker_config_patches)
-  : yamlencode(patch)]
+    })],
+  each.value.is_controlplane ? local.controlplane_config_patches : local.worker_config_patches)
 }
 
 
@@ -81,11 +97,11 @@ resource "talos_machine_bootstrap" "this" {
 
 resource "talos_cluster_kubeconfig" "this" {
   client_configuration = talos_machine_bootstrap.this.client_configuration
-  node                 = talos_machine_bootstrap.this.node
-  endpoint             = talos_machine_bootstrap.this.node
+  node                 = local.controlplane_vip
+  endpoint             = local.controlplane_vip
 
   lifecycle {
-    replace_triggered_by = [talos_machine_bootstrap.this]
+    replace_triggered_by = [talos_machine_configuration_apply.nodes]
   }
 }
 
@@ -93,5 +109,5 @@ data "talos_cluster_health" "this" {
   client_configuration = talos_machine_bootstrap.this.client_configuration
   control_plane_nodes  = [for num, node in local.nodes : node.ip if node.is_controlplane]
   worker_nodes         = [for num, node in local.nodes : node.ip if !node.is_controlplane]
-  endpoints            = [for num, node in local.nodes : node.ip if node.is_controlplane]
+  endpoints            = [talos_cluster_kubeconfig.this.endpoint]
 }
